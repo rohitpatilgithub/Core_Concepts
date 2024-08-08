fa9cdf60c9454ade6b99748e96a309fef6be8f9e
Rohit <flux.amv25@gmail.com>
2024-08-08 22:55:37 +0530

    Modified Static_Variables.java

--- Static_Variables.java
+++ Static_Variables.java
@@ -1,20 +1,18 @@
 public class Static_Variables {
-    static int variable = 10;   // this is static variable
 
-    void numberWas() {
-        int a = variable * 2;  // we can use static variable like this
-        System.out.println(a);
-    }
+    static int num = 10;    // static instance variable stored in Method area ( Meta space ) instead of heap
+    // accessing static instance variable in void method is same like normal instance variable
 
-    static void numberIs() {
-        int b = variable * 10;
-        System.out.println(b);
+    static void numAccess() { // how we can use static variable in static method ;
+        // explain static method , how can we use it ? what's purpose in real world for non static and static variables in static methods
+        Static_Variables one = new Static_Variables();
     }
 
     public static void main(String[] args) {
         Static_Variables one = new Static_Variables();
-        System.out.println(variable);   // we can directly access the static variable
-        one.numberWas();
-        numberIs();
+        one.num = 200;
+        Static_Variables two = new Static_Variables();
+        System.out.println(two.num); // here we don't get origin value (10) but we do get (200) why because
+        // static instance variables memory share same value with everyone , if it gets updated then it is same for all new objects too....
     }
 }
