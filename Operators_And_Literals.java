738ca6647c45949f139cfb04e75b2ea5baddf5dd
Rohit <flux.amv25@gmail.com>
2024-08-09 14:57:07 +0530

    Added Operators_And_Literals.java

---
+++ Operators_And_Literals.java
@@ -0,0 +1,22 @@
+public class Operators_And_Literals {
+    public static void main(String[] args) {
+
+        // OPERATORS
+
+        int a = 10;
+        int b = 2;
+        int c = 1;
+        c = a + b;   // '+' is operator, 'a' & 'b' are operands
+        c++;         // operation can be done on single operand or more
+        System.out.println(c);
+
+        // LITERALS
+
+        String name = "Rohit"; // here 'Rohit' is literal (String literal)
+        int num = 5;           // Integer '5' is Integer literal
+
+        //KEYWORDS
+
+        // int public = 10; 'public' is reserved keyword among 50 keyword ( reserved words ) in Java
+    }
+}
