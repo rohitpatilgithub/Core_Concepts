bc7c08ce81a68cf7ed462e486432f376c1c2a343
Rohit <flux.amv25@gmail.com>
2024-08-11 07:51:57 +0530

    Added Identifiers.java

---
+++ Identifiers.java
@@ -0,0 +1,15 @@
+class Maths{        // here 'Maths' is identifier ( class name )
+    int a = 10;     // here 'a' is identifier ( variable )
+    void sum() {    // here 'sum' is identifier ( method )
+        int b = a + 2;
+    }
+}
+public class Identifiers {
+    public static void main(String[] args) {
+        // Rules of identifiers are :
+        // 1.  Space cannot be used
+        // 2.  Cannot use Integer at first place / index ( 1abc ) but we can use ( a1bc )
+        // 3.  Special symbol like '$' and '_' can be used after first index
+        // 4.  Cannot use reserved keyword
+    }
+}
