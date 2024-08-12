0e56424ef3dc937cb5be94c4a4fa6c1bdc727b67
Rohit <flux.amv25@gmail.com>
2024-08-12 11:08:58 +0530

    Modified Main_Method.java

--- Main_Method.java
+++ Main_Method.java
@@ -6,5 +6,9 @@ public class Main_Method {      // class Name
         // 4. Method name ( main )
         // 5. Parameter's for the method ( String[] args )
         System.out.println("Hello world");
+        // 1. Order of access modifier can be changed : static public void ...
+        // 2. We can declare String[] args in any form : String args[] / String[] args / String []args
+        // 3. You can changed identifier name from 'args' to anything in 'String[] args'
+        // 4. We can use var args instead of 'String[] args' to 'String... args'
     }
 }
