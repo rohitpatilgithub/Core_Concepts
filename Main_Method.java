36324ad46a56e44f04f8e3057c4476fd8a9b0ee4
Rohit <flux.amv25@gmail.com>
2024-08-12 11:07:35 +0530

    Added Main_Method.java

---
+++ Main_Method.java
@@ -0,0 +1,10 @@
+public class Main_Method {      // class Name
+    public static void main(String... args) { // how ?
+        // 1. Access modifier ( public )
+        // 2. Keyword to access method without making Object ( static )
+        // 3. Keyword for method with no any return value ( void )
+        // 4. Method name ( main )
+        // 5. Parameter's for the method ( String[] args )
+        System.out.println("Hello world");
+    }
+}
