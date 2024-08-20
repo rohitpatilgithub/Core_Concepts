1b71be6f37e33d8b9cb4cc3fdbaf117aa0492946
Rohit <flux.amv25@gmail.com>
2024-08-20 08:27:25 +0530

    Added EvenOdd.java

---
+++ Chp4/Problems/EvenOdd.java
@@ -0,0 +1,16 @@
+package Chp4.Problems;
+
+public class EvenOdd {
+    public static void main(String[] args) {
+
+        // Q. Find whether number is even or odd
+
+        int a = 20;
+        if( a %2 == 0 ) {
+            System.out.println("Even");
+        }
+        else {
+            System.out.println("Odd");
+        }
+    }
+}
