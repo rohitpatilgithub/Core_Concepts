1af33b985de603b91bef5b20e7461b11c395be5e
Rohit <flux.amv25@gmail.com>
2024-09-28 19:51:45 +0530

    Added Cases of MethodOverloading.java

---
+++ Chp8/Polymorphism/MethodOverloading.java
@@ -0,0 +1,29 @@
+package Chp8.Polymorphism;
+
+public class MethodOverloading {
+
+    // For method overloading we need 3 things
+    // 1. Methods should be in same class
+    // 2. Same method name 2 or more
+    // 3. Different argument : a. type of arg
+    //                         b. no of args
+    //                         c. sequence of args
+
+    void callMe(int a) {
+        System.out.println("I am int");
+    }
+    void callMe(String a) {
+        System.out.println("I am String");
+    }
+
+    public static void main(String[] args) {
+        MethodOverloading one = new MethodOverloading();
+        one.callMe(9);
+    }
+
+    // Can we do method overloading by changing the return type
+    // No it cannot perform method overloading
+
+    // Can we do method overloading of main method
+    // Yes
+}
