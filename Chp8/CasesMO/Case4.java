3772acc75cd91b4f4536a13b371106e4f08af501
Rohit <flux.amv25@gmail.com>
2024-09-30 15:30:17 +0530

    Added Case 4 of Method overloading

---
+++ Chp8/CasesMO/Case4.java
@@ -0,0 +1,26 @@
+package Chp8.CasesMO;
+
+public class Case4 {
+    // Case 4 for Method Overloading in Polymorphism
+
+    // Case : In Method overloading
+    //        we have give
+    // Ans -> Even there's Automatic Promotion , compiler gets confused what to promote ( sequence of args ) and
+    //        which method to call any apply
+    // Condition -> Promotion of Data type in methods accordingly might get crucial if it does not perform promotion
+
+    void callMe(int a) {
+        System.out.println("Hi I'm int method");
+    }
+
+    void callMe(int... a) {
+        System.out.println("Hi I'm var args method");
+    }
+
+    public static void main(String... args) {
+        Case4 one = new Case4();
+        one.callMe(10,2,34);// var args
+        one.callMe();// var args
+        // here var args has least priority because no method shows interest then var args method gets the chance
+    }
+}
