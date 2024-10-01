dc8b8be1f2a9eeabd9af39923bbaa67a1ff04001
Rohit <flux.amv25@gmail.com>
2024-10-01 11:00:22 +0530

    Modified MethodOverloading.java

--- Chp8/Polymorphism/MethodOverloading.java
+++ Chp8/Polymorphism/MethodOverloading.java
@@ -1,13 +1,13 @@
 package Chp8.Polymorphism;
 
-public class MethodOverloading {
+//For method overloading / Compiler time polymorphism we need 3 things
+//     1. Methods should be in same class
+//     2. Same method name 2 or more
+//     3. Different argument : a. type of arg
+//                             b. no of args
+//                             c. sequence of args
 
-    // For method overloading we need 3 things
-    // 1. Methods should be in same class
-    // 2. Same method name 2 or more
-    // 3. Different argument : a. type of arg
-    //                         b. no of args
-    //                         c. sequence of args
+public class MethodOverloading {
 
     void callMe(int a) {
         System.out.println("I am int");
