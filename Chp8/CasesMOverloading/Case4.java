767836c671f6ab3074db992880da4da092189d29
Rohit <flux.amv25@gmail.com>
2024-10-02 16:06:15 +0530

    Modified Case4.java

--- Chp8/CasesMOverloading/Case4.java
+++ Chp8/CasesMOverloading/Case4.java
@@ -3,11 +3,10 @@ package Chp8.CasesMOverloading;
 public class Case4 {
     // Case 4 for Method Overloading in Polymorphism
 
-    // Case : In Method overloading
-    //        we have give
-    // Ans -> Even there's Automatic Promotion , compiler gets confused what to promote ( sequence of args ) and
-    //        which method to call any apply
-    // Condition -> Promotion of Data type in methods accordingly might get crucial if it does not perform promotion
+    // Case : In Method overloading how can we use var args
+    // Ans -> We can just use it as parameters and the conditions will decide whether it will call the var args or any variable
+    // Condition -> If there's single argument entered by user then it won't call the var args
+    //              The only time it will call var args is if we have blank argument or we have multiple arg of that data type
 
     void callMe(int a) {
         System.out.println("Hi I'm int method");
