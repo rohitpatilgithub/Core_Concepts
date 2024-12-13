cf7d0fc33a24eb231b4c968bc8b30369d0e26242
Rohit <flux.amv25@gmail.com>
2024-12-13 21:19:47 +0530

    Added SetBasic.java

---
+++ Chp11/ListAndSet/SetBasic.java
@@ -0,0 +1,33 @@
+package Chp11.ListAndSet;
+
+
+import java.util.Set;
+import java.util.HashSet;
+import java.util.Iterator;
+
+public class SetBasic {
+    public static void main(String[] args) {
+        Set one = new HashSet();
+        // Insertion method is not used here
+        // It stores using hash values not on basis of index
+        one.add(10);
+        one.add(20);
+        one.add(30);
+        System.out.println("Retrieving Object : " + one);
+        System.out.println("Iterator iterator : ");
+
+        // We can use iteration to get/retrieve the elements sequentially
+        Iterator itr = one.iterator();
+        while (itr.hasNext()) {
+            System.out.println(itr.next());
+        }
+
+        // Set cannot store multiple duplicate values
+        // It does not accept it in the Set
+        one.add(30);
+        one.add(null);
+        one.add(null);
+
+        // Set does not have its own iteration like ListIterator
+    }
+}
